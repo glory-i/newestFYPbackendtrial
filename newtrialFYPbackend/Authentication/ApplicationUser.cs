afc7c364bca2b7c7d2dd9c9b8344994f8313dd8d
Glory Iweriebor <gniweriebor@gmail.com>
2023-02-23 15:15:13 +0100

    AgeFLname to ApplicationUser

--- newtrialFYPbackend/Authentication/ApplicationUser.cs
+++ newtrialFYPbackend/Authentication/ApplicationUser.cs
@@ -4,5 +4,8 @@ namespace newtrialFYPbackend.Authentication
 {
     public class ApplicationUser : IdentityUser
     {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
